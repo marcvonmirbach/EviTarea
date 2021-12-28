3a6fc0b9cb6cd058fb85ae547c432d05a0daa38e
Marc von Mirbach <marc.mirbach@rwth-aachen.de>
2021-12-28 19:05:29 +0100

    Start add sphere movement

---
+++ Assets/Scripts/BallSpawn.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallSpawn : MonoBehaviour
+{
+    [SerializeField] private GameObject ball;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void DestroyAndSpawn()
+    {
+        
+    }
+
+}
