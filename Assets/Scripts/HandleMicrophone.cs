df127f2073dfe3c3bc87af2c4f713e87e521b7f0
Marc von Mirbach <marc.mirbach@rwth-aachen.de>
2021-12-30 13:45:56 +0100

    Fix Microphone

--- Assets/Scripts/HandleMicrophone.cs
+++ Assets/Scripts/HandleMicrophone.cs
@@ -1,44 +1,74 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HandleMicrophone : MonoBehaviour
 {
-    [SerializeField] AudioSource audioSource;
-    [SerializeField] GameObject player;
+    private AudioSource audioSource;
+    [SerializeField] Text microphoneTextField;
 
     private bool playerIsPresent;
     private bool recording;
+    private string microphone;
+    private int microphoneIdx;
 
     // Start is called before the first frame update
     void Start()
     {
+        audioSource = this.gameObject.GetComponent<AudioSource>();
         playerIsPresent = false;
         recording = false;
+        microphoneIdx = -1;
+        ChangeMicrophone();
     }
+
 
     // Update is called once per frame
     void Update()
     {
+        // record memo
         if (Input.GetKeyDown(KeyCode.M) && playerIsPresent && !recording)
         {
-            // start record
+            // start recording
             recording = true;
             Debug.Log("Start recording");
-            // TODO
-
-        } else if ((Input.GetKeyUp(KeyCode.M) || !playerIsPresent) && recording)
+            audioSource.clip = Microphone.Start(microphone, false, 60, 44100);
+            // did recording work?
+            if (Microphone.IsRecording(microphone))
+            {
+                Debug.Log("Recording started with " + microphone);
+            }
+            else
+            {
+                Debug.LogError(microphone + " doesn't work!");
+            }
+        }
+        else if ((Input.GetKeyUp(KeyCode.M) || !playerIsPresent) && recording)
         {
             // stop recording
-            Debug.Log("Stop recording" + Input.GetKeyDown(KeyCode.M) + playerIsPresent);
+            Debug.Log("Stop recording");
+            Microphone.End(microphone);
             recording = false;
-            // TODO
+        }
+
+        // change microphone
+        if (Input.GetKeyDown(KeyCode.N))
+        {
+            ChangeMicrophone();
+        }
+
+        // play recorded memo
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            audioSource.Play(); ;
         }
     }
 
     private void OnTriggerEnter(Collider collider)
     {
-        if (collider.gameObject == player)
+        if (collider.gameObject.name.Contains("Player"))
         {
             playerIsPresent = true;
             Debug.Log("Player entered the record area.");
@@ -47,10 +77,28 @@ public class HandleMicrophone : MonoBehaviour
 
     private void OnTriggerExit(Collider collider)
     {
-        if (collider.gameObject == player)
+        if (collider.gameObject.name.Contains("Player"))
         {
             playerIsPresent = false;
             Debug.Log("Player left record area.");
         }
+    }
+
+    void ChangeMicrophone()
+    {
+        // don't switch device while recording
+        if (recording)
+        {
+            return;
+        }
+        // increment device
+        microphoneIdx += 1;
+        if (microphoneIdx >= Microphone.devices.Length)
+        {
+            microphoneIdx = 0;
+        }
+        microphone = Microphone.devices[microphoneIdx];
+        // update text
+        microphoneTextField.text = microphone;
     }
 }
